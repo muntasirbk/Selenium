9da5d6d829b53a52b37f6b5ec967ec53c2206b74
Muntasir <muntasirbk@gmail.com>
2021-03-31 09:48:38 +0600

    SpiceJet Renames

--- src/SpiceJetProject.java
+++ src/SpiceJetProject.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class sp {
+public class SpiceJetProject {
 
 public static void main(String[] args) {
 // TODO Auto-generated method stub
